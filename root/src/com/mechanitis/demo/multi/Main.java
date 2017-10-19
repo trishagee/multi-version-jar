9e6584a132ce89e241dacf41deff8caebd189f5e
Trisha Gee <trisha.gee@gmail.com>
2017-10-19 16:49:16 +0200

    Simplified project structure; updated Java versions.

--- root/src/com/mechanitis/demo/multi/Main.java
+++ root/src/com/mechanitis/demo/multi/Main.java
@@ -2,9 +2,8 @@ package com.mechanitis.demo.multi;
 
 public class Main {
     public static void main(String[] args) {
-        final StackParser stackParser = new StackParser();
+        final StackInfo stackParser = new StackParser();
         System.out.println(stackParser.getStackCount());
         System.out.println(stackParser.getStack());
     }
-
 }
